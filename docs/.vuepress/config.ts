d10a527907d3aaf45d6a0b4d559f919de390ce9f
Bala Siva Sai Akhil Malepati <saiakhil2012@yahoo.com>
2022-11-16 13:20:56 -0800

    Adding new files and config for those files in vuepress

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,35 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
 
-export default defineUserConfig({
+export default {
   lang: 'en-US',
-  title: 'Hello VuePress',
-  description: 'Just playing around',
-  base: '/vmware-blockchain-samples/'
-})
+  title: 'VMware Blockchain Ethereum Developer Kit',
+  description: 'Description goes Here',
+  base: '/vmware-blockchain-samples/',
+  theme: defaultTheme({
+    navbar: [
+      // Deployment
+      {
+        text: 'Deployment',
+        link: '/deployment',
+      },
+      // Permissioning
+      {
+        text: 'Permissioning',
+        link: '/permissioning',
+      },
+      // Sample DApps
+      {
+        text: 'Sample DApps',
+        link: '/sample-dapps',
+        children: ['/sample-dapps/artemis', '/sample-dapps/erc20-swap'],
+      },
+      // Accountable Privacy
+      {
+        text: 'Accountable Privacy',
+        children: ['/accountable-privacy'],
+      },
+      // string - page file path
+      '/bar/README.md',
+    ],
+  }),
+}
